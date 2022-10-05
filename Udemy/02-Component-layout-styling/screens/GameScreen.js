3a9bab058a766d290019a12b2ed0eea62575b0ba
Kim Miso <kimms9725@naver.com>
2022-10-05 16:31:01 +0900

    feat: gamescreen 레이아웃, 전역 색 관리

--- Udemy/02-Component-layout-styling/screens/GameScreen.js
+++ Udemy/02-Component-layout-styling/screens/GameScreen.js
@@ -1,7 +1,28 @@
-import { Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import Title from '../components/Title';
 
 const GameScreen = () => {
-  return <Text>Game Screen!</Text>;
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + */}
+        {/* - */}
+      </View>
+      <View>{/*LOG ROUNDS*/}</View>
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    paddingVertical: 48,
+    paddingHorizontal: 24,
+  },
+});
