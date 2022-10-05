ce9a6863f96f3878cad2b85d0ea4bfe2f31d4131
Kim Miso <kimms9725@naver.com>
2022-10-05 17:12:40 +0900

    feat: 난수 생성

--- Udemy/02-Component-layout-styling/screens/StartGameScreen.js
+++ Udemy/02-Component-layout-styling/screens/StartGameScreen.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { TextInput, View, StyleSheet, Alert } from 'react-native';
 
-import PrimaryButton from '../components/PrimaryButtons';
+import PrimaryButton from '../components/ui/PrimaryButtons';
 import Colors from '../constants/colors';
 
 const StartGameScreen = ({ onPickNumber }) => {
