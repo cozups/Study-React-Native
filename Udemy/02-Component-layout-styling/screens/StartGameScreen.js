d0241d0484a39bc9a4efd815854bd905b3de8f9e
Kim Miso <kimms9725@naver.com>
2022-10-06 16:27:09 +0900

    feat: 화면 전환 인식

--- Udemy/02-Component-layout-styling/screens/StartGameScreen.js
+++ Udemy/02-Component-layout-styling/screens/StartGameScreen.js
@@ -1,5 +1,11 @@
 import { useState } from 'react';
-import { TextInput, View, StyleSheet, Alert } from 'react-native';
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Alert,
+  useWindowDimensions,
+} from 'react-native';
 
 import PrimaryButton from '../components/ui/PrimaryButtons';
 import Title from '../components/ui/Title';
@@ -9,6 +15,8 @@ import InstructionText from '../components/ui/InstructionText';
 
 const StartGameScreen = ({ onPickNumber }) => {
   const [enteredNumber, setEnteredNumber] = useState('');
+
+  const { width, height } = useWindowDimensions();
 
   const resetInputHandler = () => {
     setEnteredNumber('');
@@ -34,8 +42,10 @@ const StartGameScreen = ({ onPickNumber }) => {
     onPickNumber(chosenNumber);
   };
 
+  const marginTopDistance = height < 380 ? 30 : 100;
+
   return (
-    <View style={styles.rootContainer}>
+    <View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
       <Title>Guess My Number</Title>
       <Card>
         <InstructionText>Enter a Number</InstructionText>
@@ -63,10 +73,12 @@ const StartGameScreen = ({ onPickNumber }) => {
 
 export default StartGameScreen;
 
+// const deviceHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    marginTop: 100,
+    // marginTop: deviceHeight < 380 ? 30 : 100,
     alignItems: 'center',
   },
 
