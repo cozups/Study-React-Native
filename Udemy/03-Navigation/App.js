cf910d4a3f74652868685ccb94ea6508dfd406e2
Kim Miso <kimms9725@naver.com>
2022-10-07 14:28:35 +0900

    feat: food app

---
+++ Udemy/03-Navigation/App.js
@@ -0,0 +1,16 @@
+import { StyleSheet, View } from 'react-native';
+
+import CategoriesScreen from './screens/CategoriesScreen';
+
+export default function App() {
+  return <CategoriesScreen />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
