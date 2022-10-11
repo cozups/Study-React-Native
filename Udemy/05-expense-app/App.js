cee0a2795134909217c85809c633de6ad7dbd1e5
Kim Miso <kimms9725@naver.com>
2022-10-11 17:34:57 +0900

    feat: header button & 요소 누를 수 있게 하기

--- Udemy/05-expense-app/App.js
+++ Udemy/05-expense-app/App.js
@@ -7,6 +7,7 @@ import { Ionicons } from '@expo/vector-icons';
 import ManageExpense from './screens/ManageExpense';
 import RecentExpenses from './screens/RecentExpenses';
 import AllExpenses from './screens/AllExpenses';
+import IconButton from './components/UI/IconButton';
 import { GlobalStyles } from './constants/styles';
 
 const Stack = createNativeStackNavigator();
@@ -22,6 +23,14 @@ const ExpensesOverview = () => {
         headerTintColor: 'white',
         tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
         tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        headerRight: ({ tintColor }) => (
+          <IconButton
+            icon="add"
+            size={24}
+            color={tintColor}
+            onPress={() => {}}
+          />
+        ),
       }}
     >
       <BottomTabs.Screen
--- Udemy/05-expense-app/components/ExpensesOutput/ExpenseItem.js
+++ Udemy/05-expense-app/components/ExpensesOutput/ExpenseItem.js
@@ -4,8 +4,13 @@ import { GlobalStyles } from '../../constants/styles';
 import { getFormattedDate } from '../../util/date';
 
 const ExpenseItem = ({ description, date, amount }) => {
+  const expensePressHandler = () => {};
+
   return (
-    <Pressable>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItem}>
         <View>
           <Text style={[styles.textBase, styles.description]}>
@@ -24,6 +29,9 @@ const ExpenseItem = ({ description, date, amount }) => {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.75,
+  },
   expenseItem: {
     padding: 12,
     marginVertical: 8,
