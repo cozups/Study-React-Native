c3f2659677ff3cae832e69fbb7c6675785a39560
Kim Miso <kimms9725@naver.com>
2022-10-16 16:07:51 +0900

    feat: GET data from firebase

--- Udemy/05-expense-app/util/http.js
+++ Udemy/05-expense-app/util/http.js
@@ -1,8 +1,28 @@
 import axios from 'axios';
 
+const BACKEND_URL =
+  'https://react-native-course-915ec-default-rtdb.firebaseio.com';
+
 export const storeExpense = (expenseData) => {
-  axios.post(
-    'https://react-native-course-915ec-default-rtdb.firebaseio.com/expenses.json',
-    expenseData
-  );
+  axios.post(`${BACKEND_URL}/expenses.json`, expenseData);
+};
+
+export const fetchExpenses = async () => {
+  const response = await axios.get(`${BACKEND_URL}/expenses.json`);
+
+  const expenses = [];
+
+  console.log(response.data);
+
+  for (const key in response.data) {
+    const expenseObj = {
+      id: key,
+      amount: response.data[key].amount,
+      date: new Date(response.data[key].date),
+      description: response.data[key].description,
+    };
+    expenses.push(expenseObj);
+  }
+
+  return expenses;
 };
